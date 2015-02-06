e095839de3b9b71fc1de7d2c9165b7122d758db0
Albert Sison <asison89@gmail.com>
2015-02-06 14:54:41 -0800

    add BASIC CRUD functionality for Tasks

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  # get 'site/index'
-
-  # get 'site/signup'
-
-  # get 'site/login'
-
-  # get 'site/contact'
-
-  # get 'site/about'
 
   root to: 'site#index'
   get '/signup', to: 'site#signup'
@@ -15,5 +6,21 @@ Rails.application.routes.draw do
   get '/contact', to: 'site#contact'
   get '/about', to: 'site#about'
 
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+
+  post '/tasks', to: 'tasks#create'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
+
+
+
+  # get 'site/index'
+  # get 'site/signup'
+  # get 'site/login'
+  # get 'site/contact'
+  # get 'site/about'
